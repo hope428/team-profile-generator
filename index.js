4e371183222e0f0909e72a19e41d029ed90001c4
Leon Forsythe <lvforsythe95@gmail.com>
2022-12-06 17:54:11 -0500

    added all classes for each employee type. created functions to call inquirer based on employee type selection. created separate arrays of questions for inquirer

---
+++ index.js
@@ -0,0 +1,132 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+let manager;
+let engineers = [];
+let interns = [];
+
+const managerQuestions = [
+  {
+    message: "What is your manager's name?",
+    type: "input",
+    name: "name",
+  },
+  {
+    message: "What is your manager's id?",
+    type: "input",
+    name: "id",
+  },
+  {
+    message: "What is your manager's email?",
+    type: "input",
+    name: "email",
+  },
+  {
+    message: "What is your manager's office number?",
+    type: "input",
+    name: "officeNumber",
+  },
+];
+
+const engineerQuestions = [
+  {
+    message: "What is your engineer's name?",
+    type: "input",
+    name: "name",
+  },
+  {
+    message: "What is your engineer's id?",
+    type: "input",
+    name: "id",
+  },
+  {
+    message: "What is your engineer's email?",
+    type: "input",
+    name: "email",
+  },
+  {
+    message: "What is your engineer's Gitub username?",
+    type: "input",
+    name: "github",
+  },
+];
+
+const internQuestions = [
+  {
+    message: "What is your intern's name?",
+    type: "input",
+    name: "name",
+  },
+  {
+    message: "What is your intern's id?",
+    type: "input",
+    name: "id",
+  },
+  {
+    message: "What is your intern's email?",
+    type: "input",
+    name: "email",
+  },
+  {
+    message: "What is your intern's school?",
+    type: "input",
+    name: "school",
+  },
+];
+
+const addNext = [
+  {
+    message: "What type of team member would you like to add?",
+    type: "list",
+    choices: ["Engineer", "Intern", "None"],
+    name: "next",
+  },
+];
+
+function init() {
+  inquirer.prompt(managerQuestions).then((response) => {
+    manager = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.officeNumber
+    );
+    addEmployee();
+  });
+}
+
+function addEngineer() {
+  inquirer.prompt(engineerQuestions).then((res) => {
+    engineers.push(new Engineer(res.name, res.id, res.email, res.github));
+    addEmployee();
+  });
+}
+
+function addIntern() {}
+
+function endInquirer() {
+  //markup generate goes here
+  console.log("All done!");
+  console.log("Manager:");
+  console.log(manager);
+  console.log("Engineers:");
+  console.log(engineers);
+  console.log("Interns:");
+  console.log(interns);
+}
+
+function addEmployee() {
+  inquirer.prompt(addNext).then((res) => {
+    if (res.next === "Engineer") {
+      addEngineer();
+    } else if (res.next === "Intern") {
+      addIntern();
+    } else if (res.next === "None") {
+      endInquirer();
+    }
+  });
+}
+
+init();
