bb4f9822f73948fa01c79ddb812429b812238268
Leon Forsythe <lvforsythe95@gmail.com>
2022-12-07 17:27:13 -0500

    added test coverage for engineer class

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,44 @@
+const Engineer = require('../lib/Engineer')
+
+describe("Engineer", ()=>{
+    describe("Engineer params", ()=> {
+        it("should take in four params and assign them respectively", ()=>{
+            const name = "Jen"
+            const id = 342
+            const email = "jen@test"
+            const github = "jensgithub"
+
+            const engineer = new Engineer(name, id, email, github)
+
+            expect(engineer.name).toEqual(name)
+            expect(engineer.id).toEqual(id)
+            expect(engineer.email).toEqual(email)
+            expect(engineer.github).toEqual(github)
+        })
+    })
+    describe("Engineer not enough params", ()=> {
+        it("should throw an error if there are less than 4 params", ()=>{
+            const name = "Ralph"
+            const id = 243
+            const email = "ralph@test"
+
+            const cb = () => new Engineer(name, id, email)
+
+            expect(cb).toThrow()
+        })
+    })
+
+    describe("getGithub()", ()=> {
+        it("should return the engineers github name", ()=>{
+            const name = "Dave"
+            const id = 2222
+            const email = "Dave@test"
+            const github = "DaveG"
+
+            const account = new Engineer(name, id, email, github).getGithub()
+
+            expect(account).toEqual(github)
+        })
+    })
+
+})
