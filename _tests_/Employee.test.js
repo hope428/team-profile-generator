fcde0e8ab718fd5e0e00259200df704808e07801
Leon Forsythe <lvforsythe95@gmail.com>
2022-12-07 16:59:57 -0500

    added tests to employee.test.js

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,19 +1,42 @@
-const Employee = require("../lib/Employee")
+const Employee = require("../lib/Employee");
 
-describe("Employee", ()=>{
-   it("should take in 3 parameters: name, id, email", ()=>{
-    const name = "Bob"
-    const id = 1
-    const email = "bob@test"
+describe("Employee", () => {
+  describe("Employee parameters", () => {
+    it("should take in 3 parameters: name, id, email", () => {
+      const name = "Bob";
+      const id = 1;
+      const email = "bob@test";
 
-    const employee = new Employee(name, id, email)
+      const employee = new Employee(name, id, email);
 
-    expect(employee.name).toEqual(name)
-    expect(employee.id).toEqual(id)
-    expect(employee.email).toEqual(email)
-   })
+      expect(employee.name).toEqual(name);
+      expect(employee.id).toEqual(id);
+      expect(employee.email).toEqual(email);
+    });
+  });
+  describe("Employee no parameters", () => {
+    it("should throw an error if not all params are met", () => {
+      const cb = () => new Employee(1);
+      expect(cb).toThrow();
+    });
+  });
 
-   it("should throw an error if not all params are met", ()=>{
-        const cb = () => 
-   })
-})
+  describe("id is not a number", () => {
+    it("should throw an error if id is not a number", () => {
+      const cb = () => new Employee("Dan", "thirty-four", "dan@test");
+
+      const err = new Error("Id needs to be a number");
+      expect(cb).toThrow(err);
+    });
+  });
+  describe("getName method", () => {
+    it("should return string when getName is called", () => {
+      const name = "Mark";
+
+      const employee = new Employee(name, 32, "mark@test");
+      const result = employee.getName();
+
+      expect(result).toEqual(name);
+    });
+  });
+});
